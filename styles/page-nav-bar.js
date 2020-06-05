0c3029530b14d49a4b66a7c1ccfd127b947a9327
dunlike2big <dunlike2big@hotmail.com>
2020-06-04 20:31:10 -0400

    changed nav bar outlook and first page layout

--- styles/page-nav-bar.js
+++ styles/page-nav-bar.js
@@ -2,18 +2,37 @@ const navbar = document.getElementById("bar-menu-container");
 const navMenuContainer = document.getElementById("nav-menu-container");
 const navigation = document.getElementById("navigation");
 
-navbar.addEventListener("mouseenter", hideNav);
-navigation.addEventListener("mouseenter", hideNav);
-
-function hideNav() {
+navigation.addEventListener("mouseenter", showNav);
+function showNav() {
+   /*3 bar menu animation*/
+   navbar.style.transform = "rotate(-90deg)";
+   navbar.style.transformOrigin = "center";
+   navbar.style.transitionDuration = "0.5s";
+   /*navigation menu pops out and displays the various page title*/
    navMenuContainer.style.display = "block";
-
-   navigation.style.width = "45%";
+   /*expansion of circle background*/
+   navigation.style.width = "46.5%";
    navigation.style.transitionDuration = "0.75s";
-   navigation.addEventListener("mouseleave", showNav);
+
+   navigation.addEventListener("mouseleave", function hideNav() {
+      navbar.style.transform = "rotate(0deg)";
+      navbar.style.transformOrigin = "center";
+      navbar.style.transitionDuration = "0.5s";
+
+      navigation.style.width = "50px";
+      navMenuContainer.style.display = "none";
+   });
 }
 
-function showNav() {
-   navigation.style.width = "50px";
-   navMenuContainer.style.display = "none";
+const dlIconContainer = document.getElementById("download-icon-container");
+dlIconContainer.addEventListener("mouseenter", doEffects);
+function doEffects() {
+   const dlIcon = document.getElementById("download-icon");
+   const dlIconHover = document.getElementById("download-icon-hover");
+   dlIcon.style.display = "none";
+   dlIconHover.style.display = "inline-block";
+   dlIconContainer.addEventListener("mouseleave", function closeEffects() {
+      dlIcon.style.display = "inline-block";
+      dlIconHover.style.display = "none";
+   });
 }
